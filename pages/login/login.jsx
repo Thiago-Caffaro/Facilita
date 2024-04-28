e0d4126a86debd5fd8feb5963bb9e5e22a7dc3fd
Jiatsu <you@example.com>
2024-04-27 22:40:40 -0300

    início do desenvolvimento do sistema de login

--- pages/login/login.jsx
+++ pages/login/login.jsx
@@ -1,5 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { Text, View, TextInput} from 'react-native';
+import axios from 'axios';
 
 import globalStyles from '../../styles/globalStyles.js'
 import Botao from '../../components/botao/botao';
@@ -7,6 +8,29 @@ import LogoFacilita from '../../components/logoFacilita/logoFacilita.jsx';
 import ReturnArrow from '../../components/returnArrow/returnArrow.jsx';
 
 function Login({ navigation }){
+    const [matricula, setMatricula] = useState('');
+    const [senha, setSenha] = useState('');
+
+    const handleLogin = async (matricula, senha) => {
+      try {
+        const response = await axios.post('https://api.caffaro.cloud/getUser', {
+          matricula,
+          senha,
+        });
+        // Verifique a resposta do servidor e realize a validação do login.
+        if (response.data.success) {
+          // Login bem-sucedido
+          console.log('Usuário autenticado:', response.data.user);
+          navigation.navigate('Cardapio')
+        } else {
+          // Login falhou
+          console.error('Erro de autenticação:', response.data.message);
+        }
+      } catch (error) {
+        console.error('Erro ao fazer login:', error);
+      }
+    };
+
     return (
     <View style={globalStyles.container}>
       <ReturnArrow navigation={navigation}/>
@@ -18,11 +42,15 @@ function Login({ navigation }){
                   style={globalStyles.input}
                   placeholder="Matrícula"
                   placeholderTextColor="#f5f5f5"
+                  value={matricula}
+									onChangeText={setMatricula}
                   />
                   <TextInput
                   style={globalStyles.input}
                   placeholder="Senha"
                   placeholderTextColor="#f5f5f5"
+                  value={senha}
+									onChangeText={setSenha}
                   />
                   <Text
                     onPress={() => navigation.navigate('EnviarEmail')}
@@ -31,7 +59,7 @@ function Login({ navigation }){
             </View>
             <View style={globalStyles.caixaBtn}>
                   <Botao
-                    onPress={() => navigation.navigate('Cardapio')}
+                    onPress={handleLogin(matricula, senha)}
                   >Entrar</Botao>
                   {/* Colocar um onPress com caminho da pagina inicial (cardápio) */}
                   
