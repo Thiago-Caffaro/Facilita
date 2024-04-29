9ddac39b8dc3d33adb28683b374d07fd848a7973
Pedro Henrique Rodrigues <pedrututu@gmail.com>
2024-04-29 12:57:54 -0300

    Mudei apenas a cor do menu  Cardápio, pra deixar ele aceso e acrescentei um link para quando clicarem na imagem que simboliza o perfil, ir para a página 'Perfil'

--- pages/cardápio/cardapio.jsx
+++ pages/cardápio/cardapio.jsx
@@ -1,4 +1,4 @@
-import { Image,StyleSheet, Text, View} from 'react-native';
+import { Image,StyleSheet, Text, TouchableOpacity, View} from 'react-native';
 import mainWindowStyles from '../../styles/mainWindow.js'
 import Perfil from '../perfil/perfil.jsx';
 
@@ -9,8 +9,9 @@ function Cardapio({navigation}){
     return(
         <View id='container' style={mainWindowStyles.container}>
             <View id='navBar' >
+            
                 <View id='links' style={mainWindowStyles.viewNav}>
-                    <Text style={mainWindowStyles.button}>
+                    <Text style={[mainWindowStyles.button,{marginTop:-1, color:"#00FF7F", fontWeight:'bold'}]}>
                         Cardápio
                     </Text>
 
@@ -22,6 +23,7 @@ function Cardapio({navigation}){
                         Frequência
                     </Text>
                 </View>
+                <View style={styles.linhaSelecionado}/>
                 <View style={styles.linha}/>
                 
             </View>
@@ -30,18 +32,23 @@ function Cardapio({navigation}){
                
             </View>
             <View id="footer" style={styles.footer}>
-            <Image
+                <TouchableOpacity>
+                    <Image
+                        style={styles.soeSopIcon} id='soeSopIcon' source={require('../../components/logoFacilita/assets/Soe_e_Sop.png')} 
+                    />
+                </TouchableOpacity>
+
+                <TouchableOpacity onPress={() => navigation.navigate('Perfil')}>
+                    <Image 
+                        style={styles.perfilIcon} id='perfilIcon' source={require('../../components/logoFacilita/assets/Perfil.png')} 
+                    />
+                </TouchableOpacity>
                 
-                style={styles.soeSopIcon} id='soeSopIcon' source={require('../../components/logoFacilita/assets/Soe_e_Sop.png')} 
-             />
-            <Image 
-               
-                style={styles.perfilIcon} id='perfilIcon' source={require('../../components/logoFacilita/assets/Perfil.png')} 
-            />
-            <Image 
-            
-                style={styles.ajudaIcon} id='ajudaIcon' source={require('../../components/logoFacilita/assets/Ajuda.png')} 
-            />
+                <TouchableOpacity>
+                    <Image 
+                        style={styles.ajudaIcon} id='ajudaIcon' source={require('../../components/logoFacilita/assets/Ajuda.png')} 
+                    />
+                </TouchableOpacity>
             </View>
         </View>
         
@@ -53,29 +60,38 @@ const styles = StyleSheet.create({
         marginLeft:0,
         backgroundColor:'green',
         padding:50,
+        
       },
       linha:{
         borderBottomWidth: 1.5,
         borderColor: 'green',
         marginLeft:30,
         width:368,
+        borderRadius:5,
       },
       soeSopIcon:{    
-        width: 40,
+        width: 41,
         height: 40,
        marginTop:-30,
       },
       perfilIcon:{   
         marginLeft:150,  
         width: 30,
-        height: 40,
-       marginTop:-40,
+        height: 34,
+       marginTop:-35,
       },
       ajudaIcon:{   
         marginLeft:290, 
-        width: 40,
-        height: 40,
-       marginTop:-40,
+        width: 35,
+        height: 35,
+       marginTop:-35,
+      },
+      linhaSelecionado:{
+        borderBottomWidth: 2,
+        borderRadius:5,
+        borderColor: '#00FF7F',
+        marginLeft:30,
+        width:68
       }
 });
 
