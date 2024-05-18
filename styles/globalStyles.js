d8791624cf69829bc5ab24db2cdfe46070e11c40
Thiago <thiagocaffaro223@gmail.com>
2024-05-18 13:10:36 -0300

    retirei os estilos do menu de voltar daqui

--- styles/globalStyles.js
+++ styles/globalStyles.js
@@ -39,27 +39,6 @@ const globalStyles = StyleSheet.create({
         width:100,
         height:100,
     },
-    topBar:{
-        backgroundColor: '#4CAF50',
-        paddingVertical: "4%",
-        alignItems: 'center',
-        flexDirection: 'row',
-    },
-    topBarText:{
-        color: '#fff',
-        fontSize: 27,
-        fontWeight: 'bold',
-        marginTop: "5%",
-        marginLeft: "18%",
-        textAlign: 'center',
-    },
-    returnArrow:{
-        width: 20,
-        height: 20,
-        marginTop:20,
-        padding:10,
-        marginLeft: "20%",
-        tintColor: '#fff',
-    }
+    
 })
 export default globalStyles;
